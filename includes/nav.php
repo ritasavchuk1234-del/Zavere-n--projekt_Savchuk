cfbefe1f910e05f0515267c506fd533750d8e282
Marharyta Savchuk <ritasavchuk1234@gmail.com>
2026-05-17 20:40:14 +0200

    Fixed logout system and navigation

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,11 @@
+<?php session_start(); ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Japonsko</a>
+        <a class="navbar-brand" href="/Zaverečný%20projekt_Savchuk/index.php"
+
+            >Japonsko
+
+        </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
             <span class="navbar-toggler-icon"></span>
@@ -13,6 +18,20 @@
                 <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
                 <li class="nav-item"><a class="nav-link" href="galeria.php">Galéria</a></li>
                 <li class="nav-item"><a class="nav-link active" href="kontakt.php">Kontakt</a></li>
+
+                <?php if (isset($_SESSION['user'])): ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link" 
+                            href="/Zaverečný%20projekt_Savchuk/logout.php">
+
+                            Odhlásiť sa
+
+                        </a>
+
+                    </li>
+
+                <?php endif; ?>
             </ul>
         </div>
     </div>
